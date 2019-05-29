74382c8f465a06f5d706a24e48c68d50649e8912
pinguwu <premixedcloud977@gmail.com>
2019-05-28 20:23:22 -0700

    added ball draw function

--- script.js
+++ script.js
@@ -13,14 +13,24 @@ $(document).ready(function () {
   // paddle draw function
   function PaddleDraw (x, y) {
     ctx.fillStyle = "#000000";
-    ctx.fillRect(x, y, 100, 50)
+    ctx.fillRect(x, y, 100, 10);
   }
 
   // ball location starting variables
   ball.x = 200;
   ball.y = 100;
 
+  // ball draw function
+  function BallDraw (x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
   // draw the paddle using the PaddleDraw function
   PaddleDraw(paddle.x, paddle.y);
+
+  // draw the ball using the BallDraw function
+  BallDraw(ball.x, ball.y);
 
 });
