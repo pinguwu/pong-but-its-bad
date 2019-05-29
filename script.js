654e4ab861d7411bc081951872e83862254d925c
pinguwu <premixedcloud977@gmail.com>
2019-05-28 20:14:32 -0700

    turns out i didn't need the var in the object varible things

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
   var ball = new Object();
 
   // paddle location starting variables
-  var paddle.x = 150;
-  var paddle.y = 300;
+  paddle.x = 150;
+  paddle.y = 300;
 
   // defining canvas controlling variables
   var canvas = document.getElementById("pong");
@@ -16,7 +16,7 @@ $(document).ready(function () {
   }
 
   // ball location starting variables
-  var ball.x = 200;
-  var ball.y = 100;
+  ball.x = 200;
+  ball.y = 100;
 
 });
