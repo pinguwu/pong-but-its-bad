5406dfbb19f7017ce43eaed19cc8af7bdc437de7
pinguwu <premixedcloud977@gmail.com>
2019-05-29 22:39:28 -0700

    lots of stuff added

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
   // paddle location starting variables
   paddle.x = 150;
   paddle.y = 300;
+  paddle.friction = 3;
+  paddle.momentum = 0;
 
   // defining canvas controlling variables
   var canvas = document.getElementById("pong");
@@ -27,32 +29,64 @@ $(document).ready(function () {
     ctx.stroke();
   }
 
-  // gravity!
-  var gravity = 5;
-  var ballYChange = 0;
+  // starting direction determiner
+  const speeds = [-4, -3, -2, -1, 1, 2, 3, 4];
+  const otherSpeeds = [-5, 5];
+  ball.deltaX = speeds[Math.floor(Math.random() * speeds.length)];
+  if (ball.deltaX < 0) {
+    ball.deltaY = ball.deltaX + 5;
+  } else if (ball.deltaX > 0) {
+    ball.deltaY = ball.deltaX - 5;
+  }
 
-  setInterval( function () {
+  // the game loop
+  var Game = setInterval( function () {
     // clear the canvas before each pass
     ctx.clearRect(0, 0, 400, 400);
 
-    // gravity and such
-    ballYChange -= gravity;
-    if (ballYChange <= 5) {
-      ball.y -= ballYChange;
-    }
-    else {
-      ballYChange = 5;
+    // ball-paddle interaction
+    if ((ball.y) + 10 >= paddle.y && (ball.x >= paddle.x && ball.x <= (paddle.x + 100))) {
+      ball.y = paddle.y - 10;
+      ball.deltaY = -ball.deltaY;
     }
 
-    if (ball.y >= 400) {
-      ball.y = -20;
+    // ball-wall interaction
+    if (ball.x + ball.deltaX > canvas.width - 10 || ball.x + ball.deltaX < 10) {
+      ball.deltaX = -ball.deltaX;
     }
+    if (ball.y + ball.deltaY > canvas.height - 10 || ball.y + ball.deltaY < 10) {
+      ball.deltaY = -ball.deltaY;
+    }
+
+    // end the game if the ball is touching the bottom
+    if (ball.y >= 385) {
+      clearInterval(Game);
+    }
+
+    // ball movement
+    ball.y += ball.deltaY;
+    ball.x += ball.deltaX;
+
+    // paddle movement
+    document.addEventListener("keydown", function (event) {
+      if (event.defaultPrevented) {
+        return;
+      } else if (event.key == "ArrowLeft") {
+        paddle.momentum = paddle.momentum - 5;
+      } else if (event.key == "ArrowRight") {
+        paddle.momentum = paddle.momentum + 5;
+      }
+      event.preventDefault();
+    });
+    paddle.x += paddle.momentum;
+    paddle.momentum = paddle.momentum / paddle.friction
+
 
     // draw ball with each pass
     BallDraw(ball.x, ball.y);
 
     //draw paddle with each pass
     PaddleDraw(paddle.x, paddle.y);
-  }, 33);
 
+  }, 16);
 });
